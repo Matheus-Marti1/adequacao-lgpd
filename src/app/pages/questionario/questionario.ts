bcf40e617294a952f79751c319661a1edf2acf82
Matheus Marti <30555147+Matheus-Marti1@users.noreply.github.com>
2026-03-26 07:00:57 -0300

    implement initial questionnaire component

--- src/app/pages/questionario/questionario.ts
+++ src/app/pages/questionario/questionario.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+export interface PerguntaLGPD {
+  id: number;
+  artigo: string;
+  tema: string;
+  pergunta: string;
+  respostaSelecionada: "Sim" | "Não" | "Parcialmente" | null;
+  sugestaoMelhoria: string;
+}
 
 @Component({
-  selector: 'app-questionario',
-  imports: [],
-  templateUrl: './questionario.html',
-  styleUrl: './questionario.css',
+  selector: "app-questionario",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./questionario.html",
+  styleUrl: "./questionario.css",
 })
 export class Questionario {
+  listaPerguntas: PerguntaLGPD[] = [
+    {
+      id: 1,
+      artigo: "Art. 37",
+      tema: "Mapeamento de Dados",
+      pergunta: "Pergunta 1",
+      respostaSelecionada: null,
+      sugestaoMelhoria: "Sugestão 1",
+    },
+    {
+      id: 2,
+      artigo: "Art. 8º, § 1º",
+      tema: "Consentimento",
+      pergunta: "Pergunta 2",
+      respostaSelecionada: null,
+      sugestaoMelhoria: "Sugestão 2",
+    },
+    {
+      id: 3,
+      artigo: "Art. 46",
+      tema: "Segurança da Informação",
+      pergunta: "Pergunta 3",
+      respostaSelecionada: null,
+      sugestaoMelhoria: "Sugestão 3",
+    },
+  ];
 
+  finalizarAvaliacao() {
+    const perguntasSemResposta = this.listaPerguntas.filter(
+      (p) => p.respostaSelecionada === null,
+    );
+
+    if (perguntasSemResposta.length > 0) {
+      alert(
+        `Ainda faltam ${perguntasSemResposta.length} perguntas para responder.`,
+      );
+      return;
+    }
+    console.log("Respostas prontas para o relatório:", this.listaPerguntas);
+    alert(
+      "Todas as respostas registradas! Próximo passo: enviar para a tela de relatório.",
+    );
+  }
 }
