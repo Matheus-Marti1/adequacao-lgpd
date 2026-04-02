a74fc14a621f1e55656e05f9c8192ef8a20cdc7a
Matheus Marti <30555147+Matheus-Marti1@users.noreply.github.com>
2026-04-02 16:04:45 -0300

    implement home component and remove the navbar

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
+  selector: "app-home",
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: "./home.html",
+  styleUrl: "./home.css",
 })
-export class Home {
-
-}
+export class Home {}
